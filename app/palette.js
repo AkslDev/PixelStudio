ea5a6aa19713796e94330757d20e2f79812bd707
Axelcqn <Axel.cqn@gmail.com>
2016-10-07 16:49:09 +0200

    Selection des couleurs mise en place

--- app/palette.js
+++ app/palette.js
@@ -4,23 +4,40 @@ pixel_studio.palette = {
 	colors: [],
 	color_selected: null,
 
-	select_color: function(colors){
-		this.color_selected = colors;
+	select_color: function(color){
 
+		this.color_selected = color;
+
+		$('#palette a').removeClass('selected')
+				.eq(color.id)
+				.addClass('selected')
+				console.log(color.id)
 	},
 	// Création de la représentation de la palette
 	init_color: function(colors){
 		this.colors = colors;
+
+		let 	$colors 	= $('#palette'),
+			$one 		= $colors.children('a').detach();
+
 		for (var i = 0; i < colors.length; i++){
-			var 	c = $('#color').clone();
+			let c = $one.clone();
 				c.css('background-color', colors[i].to_string());
 				c.attr('title', colors[i].name);
 				$('#palette').append(c);
-		}
+				color 	= this.colors[i];
+				color.id	= i;		
+}
 
 		//Couleur par defaut
-		this.select_color(this.colors[0]);
-		console.log('Palette: colors ready');
+		this.select_color(this.colors[0]);	
+
+		// Gestion des clics
+		var self 	= this;
+		$('#palette').on('click','a',function(){
+			let index = $ ('#palette a').index(this);
+			self.select_color(self.colors[index]);
+		});
 	}
 };
 
